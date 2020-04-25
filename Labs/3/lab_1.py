2a66e055e415802fe1adf99024dcd49b46932f2b
Renato Postigo <renato.postigo@ucsp.edu.pe>
2020-04-24 22:55:08 -0500

    flex exercises

--- Labs/3/lab_1.py
+++ Labs/3/lab_1.py
@@ -1,3 +1,6 @@
+#Alumno: Renato Luis Postigo Avalos
+#Primer ejercicio regex
+
 import re
 
 ip_num = '([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])'
--- Labs/3/lab_2.py
+++ Labs/3/lab_2.py
@@ -1,7 +1,11 @@
+#Alumno: Renato Luis Postigo Avalos
+#segundo ejercicio regex
+
 import re
 
 var_regex = '[a-zA-Z][a-zA-Z0-9_]*$'
-
+#empieza en letra
+#puede tener letras, numeros o _ despues
 
 p = re.compile(var_regex)
 var = "sum_result"
