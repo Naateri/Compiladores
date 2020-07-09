253c4bdbd229d342854f041a36b31d7d840547c8
Renato Postigo <renato.postigo@ucsp.edu.pe>
2020-07-09 01:42:39 -0500

    Lab 8 completado (funciona con 1+1)

--- Labs/8/lab_8.py
+++ Labs/8/lab_8.py
@@ -7,12 +7,16 @@ Ep := + T Ep
 Ep := - T Ep
 Ep := lambda
 T  := F Tp
-Tp := * F Tp  |  /  F Tp  | lambda
+Tp := * F Tp  | lambda
 F  := ( E ) | num
 '''
 
-#from clases import E, Ep, T, Tp, F
-#from clases_t import Num
+def find_pos(array, element):
+    for i in range(len(array)):
+        if array[i] == element:
+            return i
+
+    return -1
 
 class NNTerminal:
     def __init__(self, name):
@@ -24,6 +28,23 @@ class E(NNTerminal):
         super().__init__('E')
         self.prod['T'] = None
         self.prod['Ep'] = None
+
+    def split_multi_suma(self, tokens):
+        multi = list() # T
+        suma = list() # Ep
+
+        append_multi = True
+
+        for token in tokens:
+            if token == '+' or token == '-':
+                append_multi = False
+            
+            if append_multi:
+                multi.append(token)
+            else:
+                suma.append(token)
+
+        return multi, suma
 
     def interpret(self, text):
 
@@ -42,14 +63,22 @@ class E(NNTerminal):
 
         self.prod['Ep'] = Ep()
 
-        # Ep: suma o resta
-        # text[0]: anterior
-        # text[1]: resto de la operacion incluyendo el operador
+        multi, suma = self.split_multi_suma(cur_text)
+        #print('multi', multi)
+        #print('suma', suma)
 
-        if len(cur_text) > 1: # Suma o resta
-            return self.prod['Ep'].interpret(cur_text[0], cur_text[1:])
+        # Ep: suma o resta
+        # text[0] (multi): anterior, interpretado por T
+        # text[1] (suma): resto de la operacion incluyendo el operador
+        # Ep es el encargado de interpretar a T (anterior)
+
+        #if len(cur_text) > 1:
+        if len(suma) > 1: # Suma o resta
+            #return self.prod['Ep'].interpret(cur_text[0], cur_text[1:])
+            return self.prod['Ep'].interpret(multi, suma)
         else: # Pasar cadena vacía (lambda)
-            return self.prod['Ep'].interpret(cur_text[0], '')
+            #return self.prod['Ep'].interpret(cur_text[0], '')
+            return self.prod['Ep'].interpret(multi, '')
 
 
 class Ep(NNTerminal):
@@ -61,12 +90,15 @@ class Ep(NNTerminal):
         self.prod['Ep'] = None
         self.prod['lambda'] = None
 
+    # anterior: suma acumulada
+    # text: lo que sigue
     def interpret(self, anterior, text):
         self.prod['T'] = T()
         valorT = self.prod['T'].interpret(anterior)
 
         self.prod['Ep'] = Ep()
-
+        
+        # creacion de instancia según sea necesario
         if text == '':
             self.prod['lambda'] = Lambda()
         elif text[0] == '+':
@@ -76,18 +108,47 @@ class Ep(NNTerminal):
         
         if self.prod['+'] != None:
             print('+ T Ep')
-            #suma = int(anterior) + valorT
             self.prod['+'].interpret()
+            print('Ep text', text)
+            # si sigue más texto: pasarlo
             if len(text) > 2:
-                return valorT + self.prod['Ep'].interpret(text[1], text[2:])
+                # Si hay más sumas o restas, Ep no es lambda
+                if '+' in text[1:] or '-' in text[1:]:
+                    # buscar + o - para saber que enviar
+                    # consideracion: parte a la izquierda de la suma = T
+                    op_pos = find_pos(text[1:], '+')
+                    # no encontro +, probar con -
+                    if op_pos == -1:
+                        op_pos = find_pos(text[1:], '-')
+                    return valorT + self.prod['Ep'].interpret(text[1], text[2:])
+                else:
+                # Si no quedan sumas o restas, Ep es lambda
+                    return valorT + self.prod['Ep'].interpret(text[1:], '')
+            # si no, pasar cadena vacía
             else:
                 return valorT + self.prod['Ep'].interpret(text[1], '')
 
         elif self.prod['-'] != None:
             print('- T Ep')
-            resta = int(anterior) - valorT
-            return self.prod['Ep'].interpret(resta)
-
+            self.prod['-'].interpret()
+            # si sigue más texto: pasarlo
+            if len(text) > 2:
+                # Si hay más sumas o restas, Ep no es lambda
+                if '+' in text[1:] or '-' in text[1:]:
+                    # buscar + o - para saber que enviar
+                    # consideracion: parte a la izquierda de la suma = T
+                    op_pos = find_pos(text[1:], '+')
+                    # no encontro +, probar con -
+                    if op_pos == -1:
+                        op_pos = find_pos(text[1:], '-')
+                    return valorT - self.prod['Ep'].interpret(text[1], text[2:])
+                else:
+                # Si no quedan sumas o restas, Ep es lambda
+                    return valorT - self.prod['Ep'].interpret(text[1:], '')
+            # si no, pasar cadena vacía
+            else:
+                return valorT - self.prod['Ep'].interpret(text[1], '')
+        # si no hay mas texto, retornar lo acumulado
         elif self.prod['lambda'] != None:
             #print('lambda')
             self.prod['lambda'].interpret()
@@ -99,6 +160,23 @@ class T(NNTerminal):
         self.prod['F'] = None
         self.prod['Tp'] = None
 
+    def split_multi_num(self, tokens):
+        num = list() # F
+        multi = list() # Tp
+
+        append_num = True
+
+        for token in tokens:
+            if token == '*':
+                append_num = False
+            
+            if append_num:
+                num.append(token)
+            else:
+                multi.append(token)
+
+        return num, multi
+
     def interpret(self, text):
         cur_f = F()
 
@@ -108,19 +186,34 @@ class T(NNTerminal):
 
         self.prod['Tp'] = Tp()
 
-        cur_text = text.split()
+        if type(text) != list:
+            cur_text = text.split()
+        else:
+            cur_text = text
 
-        if len(cur_text) > 1: # Multiplicacion
-            return self.prod['Tp'].interpret(cur_text[0], cur_text[1:])
+        num, multi = self.split_multi_num(cur_text)
+
+        #print('num', num)
+        #print('multi', multi)
+
+        # Tp: multiplicacion o division
+        # text[0] (num): anterior, interpretado por F
+        # text[1] (multi): resto de la operacion incluyendo el operador
+        # Tp es el encargado de interpretar a F (anterior)
+
+        #if len(cur_text) > 1: # Multiplicacion
+        if len(multi) > 1: # Multiplicacion
+            #return self.prod['Tp'].interpret(cur_text[0], cur_text[1:])
+            return self.prod['Tp'].interpret(num[0], multi)
         else: # Pasar cadena vacía (lambda), posible numero
-            return self.prod['Tp'].interpret(cur_text[0], '')
+            #return self.prod['Tp'].interpret(cur_text[0], '')
+            return self.prod['Tp'].interpret(num[0], '')
         
 
 class Tp(NNTerminal):
     def __init__(self):
         super().__init__('Tp')
         self.prod['*'] = None
-        self.prod['/'] = None
         self.prod['F'] = None
         self.prod['Tp'] = None
         self.prod['lambda'] = None
@@ -129,22 +222,28 @@ class Tp(NNTerminal):
         self.prod['F'] = F()
         valorF = self.prod['F'].interpret(anterior)
 
+        self.prod['Tp'] = Tp()
+
         if text == '':
             self.prod['lambda'] = Lambda()
         elif text[0] == '*':
             self.prod['*'] = Product()
-        elif text[0] == '':
-            self.prod['lambda'] = Lambda()
 
         if self.prod['*'] != None:
-            multi = anterior * valorF
-            return self.prod['Tp'].interpret(multi)
+            print('* F Tp')
+            #suma = int(anterior) + valorT
+            self.prod['*'].interpret()
+            # si sigue más texto: pasarlo
+            if len(text) > 2:
+                return valorF * self.prod['Tp'].interpret(text[1], text[2:])
+            # si no, pasar cadena vacía
+            else:
+                return valorF * self.prod['Tp'].interpret(text[1], '')
 
-        elif self.prod['/'] != None:
-            divi = anterior / valorF
-            return self.prod['Tp'].interpret(divi)
-
+        # si no hay mas texto, retornar lo acumulado
         elif self.prod['lambda'] != None:
+            #print('lambda')
+            self.prod['lambda'].interpret()
             return valorF
 
 class F(NNTerminal):
@@ -200,8 +299,10 @@ class Lambda(Terminal):
 
 def main():
     
-    cadena = '10 + 15'
-    #cadena = 'num + num'
+    #cadena = '10 * 2 + 2'
+    #cadena = '10 + 2 * 2'
+    cadena = '1 + 1'
+    #cadena = '2 * 3'
     temp = E()
     print(cadena, '=', temp.interpret(cadena))
 
